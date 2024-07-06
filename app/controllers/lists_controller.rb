f52c5a894746090ca6500d08a971fccfeb215623
Ramone Robertson <ramonerobertson10@gmail.com>
2024-07-06 16:44:22 +0900

    Finished create list action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,8 +11,21 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    @list = List.create(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
