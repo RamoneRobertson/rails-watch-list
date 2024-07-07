d99d75e6bee4ba08cf6a6e5ffca56b35f5249e83
Ramone Robertson <ramonerobertson10@gmail.com>
2024-07-07 17:39:08 +0900

    finished create bookmark action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,18 +1,30 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create, :destroy]
   def new
     @bookmark = Bookmark.new
   end
 
   def create
-    raise
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = @list.id
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.Find(params[:id])
+    @bookmark.destroy
   end
 
   private
   def set_list
-    @list = List.find(params[list_id])
+    @list = List.find(params[:list_id])
   end
 
   def bookmark_params
-    params.require(:list)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
