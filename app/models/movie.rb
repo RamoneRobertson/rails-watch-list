38364dd1b9112e4c96cb4dda1c5c8acd2a3895af
Ramone Robertson <ramonerobertson10@gmail.com>
2024-07-10 11:48:32 +0900

    Finished seeding for heroku

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  # validates :title, :overview, uniqueness: true
+  validates :title, :overview, uniqueness: true
   validates :title, :overview, presence: true
 end
