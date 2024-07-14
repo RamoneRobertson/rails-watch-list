6d4bf695ceb2b2165dd7c33bda19c9cfa86c189b
Ramone Robertson <ramonerobertson10@gmail.com>
2024-07-14 14:00:39 +0900

    Added strict loading for bookmark and movies in list model association

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,7 +5,7 @@ class ListsController < ApplicationController
   end
 
   def show
-    @movies = @list.movies
+    @bookmarks = @list.bookmarks
   end
 
   def new
@@ -23,7 +23,7 @@ class ListsController < ApplicationController
 
   private
   def set_list
-    @list = List.find(params[:id])
+    @list = List.includes(bookmarks: :movie).find(params[:id])
   end
 
   def list_params
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
-  has_many :bookmarks, dependent: :destroy
-  has_many :movies, through: :bookmarks
+  has_many :bookmarks, dependent: :destroy, strict_loading: true
+  has_many :movies, through: :bookmarks, strict_loading: true
   # list.rb
   has_one_attached :photo
 
